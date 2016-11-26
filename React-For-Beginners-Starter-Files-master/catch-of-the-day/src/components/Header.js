ee92c2a3e3c2671f210185a5e59b56a311487d92
michael burney <burney318@gmail.com>
2016-11-26 16:12:08 -0600

    finished session 7 (Passing Dynamic data with props)

--- React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/Header.js
+++ React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/Header.js
@@ -3,7 +3,17 @@ import React from 'react';
 class Header extends React.Component{
     render(){
         return(
-            <p>Header</p>
+            <header className="top">
+                <h1>
+                    <span className="Catch">Catch</span>
+                    <span className="ofThe">
+                        <span className="of">of</span>
+                        <span className="the">the</span>
+                    </span>
+                    <span className="Day">Day</span>
+                </h1>
+                <h3 className="tagline">{this.props.tagline}</h3>
+            </header>
         )
     }
 }
