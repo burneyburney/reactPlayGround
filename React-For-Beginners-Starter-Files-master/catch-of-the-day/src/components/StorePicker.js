e4192e043cbdac38966a97c7021dd9881a21f076
michael burney <burney318@gmail.com>
2016-11-26 01:41:55 -0600

    made my first component

---
+++ React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class StorePicker extends React.Component { // create component, cap first componenet name so it can be used more than once
+    render(){
+        return(
+            <form className="store-selector">
+                <p>Hello</p>
+            </form>//can only return 1 parent element
+        )
+    }
+}
+
+export default StorePicker;
--- React-For-Beginners-Starter-Files-master/catch-of-the-day/src/index.js
+++ React-For-Beginners-Starter-Files-master/catch-of-the-day/src/index.js
@@ -1,1 +1,5 @@
-// let's go!
+import React from 'react'; // React = variables, 'react' = what we are making it
+import {render} from 'react-dom'; //take one method from react-dom
+import StorePicker from './components/StorePicker';
+
+render(<StorePicker/>, document.querySelector('#main'));
