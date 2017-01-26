eb59180dba6eeaec2b48b7d1694d1758b38f70c1
michael burney <burney318@gmail.com>
2017-01-25 20:11:57 -0600

    deleted react 8(

--- React-For-Beginners-Starter-Files-master/catch-of-the-day/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'; // React = variables, 'react' = what we are making it
-import {render} from 'react-dom'; //take one method from react-dom
-import './css/style.css';
-import App from './components/App';
-
-import StorePicker from './components/StorePicker';
-
-render(<App />, document.querySelector('#main'));
